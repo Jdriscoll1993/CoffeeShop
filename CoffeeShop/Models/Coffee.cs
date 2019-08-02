505fab79c66cad6a37e77383aef8bbedacb64629
Joey <Driscoll.Recording@gmail.com>
2019-08-01 21:25:37 -0700

    first commit

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Bean Type way too long")]
+        [Required]
+        [IgnoreDataMember]
+        public string BeanType { get; set; }
+
+    }
+}
---
+++ CoffeeShop/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+        public Person Parent { get; set; }
+    }
+}
